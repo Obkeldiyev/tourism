fc12c5f7ac60330c6f4ca824f0e953a2fde43e1d
Obkeldiyev <obkeldiyevgiga13@gmail.com>
2026-01-14 10:16:55 +0500

    Ready version

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -4,8 +4,11 @@ import { verifyAdmin } from "src/middlewares/verifyAdmin";
 
 const adminRoutes = Router();
 
+adminRoutes.post("/login", AdminController.login);
 adminRoutes.get("/", verifyAdmin, AdminController.getAllAdmins);
 adminRoutes.post("/", verifyAdmin, AdminController.createAdmin);
 adminRoutes.get("/account", verifyAdmin, AdminController.getAccount);
+adminRoutes.patch("/password", verifyAdmin, AdminController.editPassword);
+adminRoutes.patch("/username", verifyAdmin, AdminController.editUsername);
 
 export { adminRoutes };
